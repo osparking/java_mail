c9d1c4edb3c155589a9793ce7331a09ca530fb8d
Park,JongBum <jbpark03@gmail.com>
2021-09-04 05:48:48 +0900

    Trial version of Solution for the 1st problem
    of 12th exam.

--- src/main/java/mail/MailTest.java
+++ src/main/java/mail/MailTest.java
@@ -2,6 +2,7 @@
 package mail;
 
 import java.io.Console;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -10,12 +11,16 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
 
+import javax.mail.BodyPart;
 import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * This program shows how to use JavaMail to send mail messages.
@@ -24,47 +29,75 @@ import javax.mail.internet.MimeMessage;
  * @version 1.01 2018-03-17
  */
 public class MailTest {
-	public static void main(String[] args) throws MessagingException, IOException {
+	public static void main(String[] args) {
 		var props = new Properties();
-		
+
 		/**
-		 * MailTest\src\main\java
-		 * - MailTest : 프로젝트 폴더
-		 * - src\main\java : 작업 디렉토리 // 상대주소의 base 디렉토리
+		 * 프로젝트 이름: MailTest
+		 * 프로젝트 폴더: MailTest\src\main\java
+		 * 작업 디렉토리: src\main\java
+		 * 상대주소의 base 디렉토리
 		 */
-		// 아래 첫 실인자 값인 "mail"은 작업 디렉토리에 있는 경로명의 전반부
-		// "mail.properties"은 작업 디렉토리에 있는 경로명의 후반부
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-
-		try (InputStream in = Files.newInputStream(Paths.get("mail", "mail.properties"))) {
-			props.load(in);
-		}
-		List<String> lines = Files.readAllLines(Paths.get(args[0]), StandardCharsets.UTF_8);
-
-		String from = lines.get(0);
-		String to = lines.get(1);
-		String subject = lines.get(2);
-
-		var builder = new StringBuilder();
-		for (int i = 3; i < lines.size(); i++) {
-			builder.append(lines.get(i));
-			builder.append("\n");
-		}
-
-		Console console = System.console();
-		String password = new String(console.readPassword("Password: "));
-
+		System.out.println("작업 디렉토리 = " + 
+							System.getProperty("user.dir"));
+		
 		Session mailSession = Session.getDefaultInstance(props);
-		// mailSession.setDebug(true);
-		var message = new MimeMessage(mailSession);
-		message.setFrom(new InternetAddress(from));
-		message.addRecipient(RecipientType.TO, new InternetAddress(to));
-		message.setSubject(subject);
-		message.setText(builder.toString());
-
-		try (Transport tr = mailSession.getTransport()) {
-			tr.connect("jong479", password);
+		/**
+		 *  아래 첫 실인자 "mail": 작업 디렉토리 내 상대 경로명 전반부
+		 *  "mail.properties": 작업 디렉토리 내 상대 경로명 후반부 
+		 */
+		try (InputStream in = Files.newInputStream(
+								Paths.get("mail", "mail.properties"));
+				Transport tr = mailSession.getTransport()) {
+			
+			props.load(in);
+			
+			List<String> lines;
+			int line = 0;
+			lines = Files.readAllLines(Paths.get(args[0]), StandardCharsets.UTF_8);
+			String from = lines.get(line++);
+			String to1 = lines.get(line++);
+			String to2 = lines.get(line++);
+			String subject = lines.get(line++);
+			
+			var builder = new StringBuilder();
+			for (int i = line++; i < lines.size(); i++) {
+				builder.append(lines.get(i));
+				builder.append("\n");
+			}
+			
+			var message = new MimeMessage(mailSession);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(RecipientType.TO, new InternetAddress(to1));
+			message.addRecipient(RecipientType.CC, new InternetAddress(to2));
+			message.setSubject(subject);
+			addBodyParts(message, builder);
+			
+			Console console = System.console();
+			String password = new String(console.readPassword("Password: "));			
+			
+			tr.connect("jbpark03", password);
 			tr.sendMessage(message, message.getAllRecipients());
-		}
+			System.out.println("메일 전송 성공!");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	private static void addBodyParts(MimeMessage message, 
+			StringBuilder builder) 
+					throws MessagingException, IOException {
+		Multipart multipart = new MimeMultipart();
+	
+		BodyPart messageBodyPart = new MimeBodyPart(); 
+		messageBodyPart.setText(builder.toString());
+		multipart.addBodyPart(messageBodyPart);
+		
+		MimeBodyPart attachmentPart = new MimeBodyPart();
+		attachmentPart.attachFile(new File("letter.txt")); // "C:\\Document1.txt";
+		multipart.addBodyPart(attachmentPart);
+		message.setContent(multipart);
 	}
 }
